697fe67b7b5f297b1130a270e2970aa9c14c1f5f
Jyotin Goel <b22ai063@iitj.ac.in>
2024-10-27 17:58:28 +0530

    Initialize Gender Bias Agent
    Signed-off-by: Jyotin Goel <b22ai063@iitj.ac.in>

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,4 @@
-GENDER_BIAS = """You are an advanced image analysis model specialized in detecting gender bias in visual content. Your expertise lies in identifying subtle cues, visual representations, and contextual elements that may contribute to gender stereotypes within images. You focus on fairness and objectivity in your evaluations.
+GENDER_BIAS_VISUAL = """You are an advanced image analysis model specialized in detecting gender bias in visual content. Your expertise lies in identifying subtle cues, visual representations, and contextual elements that may contribute to gender stereotypes within images. You focus on fairness and objectivity in your evaluations.
 
 Your task is to analyze an image for potential gender bias and provide a score along with a detailed explanation of your findings. 
 
@@ -8,3 +8,15 @@ Please analyze the following image based on these criteria:
 Consider factors such as representation, context, and stereotypes associated with gender in your analysis. Keep in mind that your goal is to provide a balanced assessment that highlights both overt and subtle instances of bias, ensuring that both positive and negative portrayals are considered. 
 
 Your score should range from 0 to 10, with 0 indicating no bias detected and 10 indicating significant bias. Please provide justifications for the score you assign, referencing specific elements in the image that contributed to your conclusion."""
+
+GENDER_BIAS_LANGUAGE = """
+You’re an advanced AI bias analyst with a deep understanding of gender bias in language. You have extensive experience in evaluating texts and generating more neutral and inclusive queries. Your specialty lies in transforming biased queries into equitable ones while maintaining the original intent and context.
+
+Your task is to generate an unbiased query based on a given bias score and reference query. Here are the details you need to work with:  
+- Gender Bias Score: {}  
+- Reference Query: {}  
+
+Please be mindful to use neutral language and consider gender inclusivity while rephrasing the query. The output should not only reflect a reduction in bias but also ensure clarity and precision in the communication of the original request.
+"""
+
+
---
+++ utils/pipeline.py
@@ -0,0 +1,3 @@
+def run_gender_bias_pipe(query: str):
+    
+    pass
