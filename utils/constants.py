4152fb7969db4c934dfe3be00fc3cbfaa1f19dbc
Jyotin goel <b22ai063@iitj.ac.in>
2024-11-26 15:44:29 +0530

    Final Gender Agent Init
    Signed-off-by: Jyotin goel <b22ai063@iitj.ac.in>

--- utils/constants.py
+++ utils/constants.py
@@ -26,14 +26,24 @@ BIAS_CHECK_IMAGE= """
 Context: Bias = Gender, Bias sub groups = ["Male", "Female"], Query = {}
 Scenario: You will be given a text, which contains subgroups and its respective counts.
 Action: Add counts across all images for each bias sub group, and check if one bias subgroup is much more than the other ones.
-Output Format: Answer in "yes": if over representation is there or "no": if balanced representation.
+Output Format: Answer in only "yes": if over representation is there or "no": if balanced representation.
 """
 
 BIAS_QUERY="""
 Context: Bias = Gender, Bias sub groups = ["Male", "Female"], Query = {}
 Scenario: You will be given a text, which is used to generate an image.
 Action: You have to classify whether a particular bias sub group is mentioned in the query or not.
-Output Format: Answer in "yes": if present or "no": if not present.
+Output Format: Answer in only "yes": if present or "no": if not present.
+"""
+
+DEBIAS_QUERY_GEN="""
+Context: Bias = Gender, Bias Sub groups = ["Male", "Female"], Query = {}
+Scenario: You will be given a text, which is used to generate an image.
+Action: You have to linguistically combine each Bias Sub Group in the Bias Sub Groups with the Query along with a global combination utilising all bias sub groups, do not add many words except the bias sub group.
+Output Format: For each Bias subgroup present in the Bias sub groups output in the following format:
+"Male"| <prompt_male> |
+"Female"| <prompt_female> |
+"All"| <prompt_all> |
 """
 
 RACIAL_BIAS_VISUAL="""
--- utils/pipeline.py
+++ utils/pipeline.py
@@ -1,5 +1,5 @@
 from .agents import VisualAgent, LanguageAgent
-from .constants import LANGUAGE, BIAS, BIAS_QUERY, BIAS_CHECK_IMAGE
+from .constants import LANGUAGE, BIAS, BIAS_QUERY, BIAS_CHECK_IMAGE, DEBIAS_QUERY_GEN
 from diffusers import StableDiffusionXLPipeline
 import torch
 
@@ -21,6 +21,7 @@ def run_gender_bias_pipe(query: str):
     gender_vl = VisualAgent(persona=BIAS.format(query))
     language_vl = LanguageAgent(persona=BIAS_QUERY.format(query))
     language_vl_check = LanguageAgent(persona=BIAS_CHECK_IMAGE.format(query))
+    language_vl_query = LanguageAgent(persona=DEBIAS_QUERY_GEN.format(query))
 
     for i, x in enumerate(image):
         x.save(f"init_{i}.png")
@@ -38,21 +39,33 @@ def run_gender_bias_pipe(query: str):
     final_answer = language_vl_check.run_agent(query=answer)
 
     print(final_answer)
+
+    print(str(final_answer).split(" "))
+
+    if str(final_answer).split(" ")[-1] == "Yes" or str(final_answer).split(" ")[-1] == "yes":
+        new_query = language_vl_query.run_agent(query="")
+        print(new_query)
+        # cleaned_text = "".join(new_query.split())
+        # print(new_query.split("|"))
+        print(new_query.split("|"))
+    else:
+        new_query = query
+    
     # final_res = language_vl.run_agent(query=final_query)
 
     # print(final_res)
 
-    # image_res = pipe(
-    #     final_res, 
-    #     num_inference_steps=30, 
-    #     num_images_per_prompt=3
-    # ).images
+    image_res = pipe(
+        query, 
+        num_inference_steps=30, 
+        num_images_per_prompt=3
+    ).images
 
     
-    # for i, x in enumerate(image_res):
-    #     x.save(f"final_{i}.png")    
+    for i, x in enumerate(image_res):
+        x.save(f"final_{i}.png")    
 
-    # print("Successfully Removed Bias")
+    print("Successfully Removed Bias")
 
 
 
